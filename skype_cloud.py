c2d5f9b04ba30d2091b26ff6b71434e3f0a11373
obnoxious <obnoxious@dongcorp.org>
2014-10-27 21:39:18 -0700

    haha

---
+++ skype_cloud.py
@@ -0,0 +1,17 @@
+import requests
+import sys
+
+class cloud(object):
+    def __init__(self):
+        self.oldRes = []
+        self.originalRequest = requests.get('http://gibson.tgqx.at/api/%s'%sys.argv[1]).json()
+        for result in self.originalRequest:
+            print('[!] Original: %s'%result['public'])
+            self.oldRes.append(result['public'])
+        while True:
+            for result in requests.get('http://gibson.tgqx.at/api/%s'%sys.argv[1]).json():
+                if result['public'] not in self.oldRes:
+                    print("[+] New IP: %s"%result['public'])
+
+if __name__ == "__main__":
+    cloud()
